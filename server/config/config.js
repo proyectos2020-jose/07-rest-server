bfa73bf72f21fc0a6638178b4e3365c24a2183fa
Jose <josevicente.rincon@redegal.com>
2020-04-20 18:22:12 +0200

    Configuración entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Configuracion mongo
-process.env.url_db = (process.env.NODE_ENV !== 'dev') ? 'mongodb+srv://jose:l9tbKq8LVTHanSpj@cluster0-27vq3.mongodb.net/cafe' : 'mongodb://localhost:27017/cafe';
+process.env.url_db = (process.env.NODE_ENV !== 'dev') ? process.env.MONGO_URI : 'mongodb://localhost:27017/cafe';
